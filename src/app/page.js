679bfd04e1a2cfd51ea1d2d825293ac728f0ecb6
Huslen1027 <hvslenhvslen566@gmail.com>
2024-07-26 17:32:21 +0800

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+import Landing from "../components/landing";
+import Project from "../components/projects";
+import styles from "./page.module.scss";
 export default function Home() {
-  return <div></div>;
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+  return (
+    <main className={styles.main}>
+      <Landing />
+      <Project />
+    </main>
+  );
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+"use client";
+import style from "./style.module.scss";
+import Nav from "./nav";
+import { useState } from "react";
+
+const Index = () => {
+  const [isActive, setIsActive] = useState(false);
+  return <div></div>;
+};
+
+export default Index;
