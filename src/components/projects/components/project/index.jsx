c6316d4df6a9e078f724e82fd60a72e8d332ad59
Huslen1027 <hvslenhvslen566@gmail.com>
2024-08-03 11:40:58 +0800

    mai

--- src/components/projects/components/project/index.jsx
+++ src/components/projects/components/project/index.jsx
@@ -1,10 +1,18 @@
 "use client";
 import styles from "./style.module.scss";
 import React from "react";
-export default function Index({ index, title, manageModla }) {
+export default function Index({ index, title, manageModal }) {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div
+      onMouseEnter={(e) => {
+        manageModal(true, index, e.clientX, e.clientY);
+      }}
+      onMouseLeave={(e) => {
+        manageModal(false, index, e.clientX, e.clientY);
+      }}
+      className={styles.project}
+    >
+      <h2>{title}</h2>
       <p>Front-end Developer</p>
     </div>
   );
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,21 +1,21 @@
 "use client";
-
 import styles from "./style.module.scss";
-import Image from "next/image";
-import { motion } from "framer-motion";
-import Project from "./components/project";
 import { useState, useEffect, useRef } from "react";
+import Project from "./components/project";
+import { motion } from "framer-motion";
 import gsap from "gsap";
+import Image from "next/image";
+import Link from "next/link";
 const projects = [
-  {
-    title: "Furniro",
-    src: "furniro.jpg",
-    colo: "#706D63",
-  },
   {
     title: "Food-delivery",
     src: "foodDelivery.jpg",
     color: "#000000",
+  },
+  {
+    title: "Furniro",
+    src: "furniro.jpg",
+    colo: "#706D63",
   },
   {
     title: "Blog",
@@ -29,12 +29,28 @@ const projects = [
   },
 ];
 
+const scaleAnimation = {
+  initial: { scale: 0, x: "-50%", y: "-50%" },
+  enter: {
+    scale: 1,
+    x: "-50%",
+    y: "-50%",
+    transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] },
+  },
+  closed: {
+    scale: 0,
+    x: "-50%",
+    y: "-50%",
+    transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
+  },
+};
+
 export default function Home() {
-  const cursor = useRef(null);
-  const cursorlabel = useRef(null);
-  const modalContainer = useRef(null);
   const [modal, setModal] = useState({ active: false, index: 0 });
   const { active, index } = modal;
+  const modalContainer = useRef(null);
+  const cursor = useRef(null);
+  const cursorLabel = useRef(null);
 
   let xMoveContainer = useRef(null);
   let yMoveContainer = useRef(null);
@@ -43,23 +59,88 @@ export default function Home() {
   let xMoveCursorLabel = useRef(null);
   let yMoveCursorLabel = useRef(null);
 
+  useEffect(() => {
+    //Move Container
+    xMoveContainer.current = gsap.quickTo(modalContainer.current, "left", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    yMoveContainer.current = gsap.quickTo(modalContainer.current, "top", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    //Move cursor
+    xMoveCursor.current = gsap.quickTo(cursor.current, "left", {
+      duration: 0.5,
+      ease: "power3",
+    });
+    yMoveCursor.current = gsap.quickTo(cursor.current, "top", {
+      duration: 0.5,
+      ease: "power3",
+    });
+    //Move cursor label
+    xMoveCursorLabel.current = gsap.quickTo(cursorLabel.current, "left", {
+      duration: 0.45,
+      ease: "power3",
+    });
+    yMoveCursorLabel.current = gsap.quickTo(cursorLabel.current, "top", {
+      duration: 0.45,
+      ease: "power3",
+    });
+  }, []);
+
+  const moveItems = (x, y) => {
+    xMoveContainer.current(x);
+    yMoveContainer.current(y);
+    xMoveCursor.current(x);
+    yMoveCursor.current(y);
+    xMoveCursorLabel.current(x);
+    yMoveCursorLabel.current(y);
+  };
+  const manageModal = (active, index, x, y) => {
+    moveItems(x, y);
+    setModal({ active, index });
+  };
+
   return (
-    <main className={styles.projects}>
+    <main
+      onMouseMove={(e) => {
+        moveItems(e.clientX, e.clientY);
+      }}
+      className={styles.projects}
+    >
       <div className={styles.body}>
         {projects.map((project, index) => {
-          return <Project index={index} key={index} title={project.title} />;
+          return (
+            <Project
+              index={index}
+              title={project.title}
+              manageModal={manageModal}
+              key={index}
+            />
+          );
         })}
       </div>
+
       <>
-        <motion.div ref={modalContainer} className={styles.modalContainer}>
-          <div>
+        <motion.div
+          ref={modalContainer}
+          variants={scaleAnimation}
+          initial="initial"
+          animate={active ? "enter" : "closed"}
+          className={styles.modalContainer}
+        >
+          <div
+            style={{ top: index * -100 + "%" }}
+            className={styles.modalSlider}
+          >
             {projects.map((project, index) => {
               const { src, color } = project;
               return (
                 <div
                   className={styles.modal}
                   style={{ backgroundColor: color }}
-                  key={index}
+                  key={`modal_${index}`}
                 >
                   <Image
                     src={`/images/${src}`}
@@ -71,6 +152,22 @@ export default function Home() {
               );
             })}
           </div>
+        </motion.div>
+        <motion.div
+          ref={cursor}
+          className={styles.cursor}
+          variants={scaleAnimation}
+          initial="initial"
+          animate={active ? "enter" : "closed"}
+        ></motion.div>
+        <motion.div
+          ref={cursorLabel}
+          className={styles.cursorLabel}
+          variants={scaleAnimation}
+          initial="initial"
+          animate={active ? "enter" : "closed"}
+        >
+          View
         </motion.div>
       </>
     </main>
